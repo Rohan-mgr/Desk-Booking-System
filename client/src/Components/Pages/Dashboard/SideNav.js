03f9422abccccee4aef2cdb61f1824d2b095fc50
Rohan Magar <rohan.magar.415@gmail.com>
2022-10-21 21:42:56 +0545

    added user dashboard

--- client/src/Components/Pages/Dashboard/SideNav.js
+++ client/src/Components/Pages/Dashboard/SideNav.js
@@ -14,7 +14,6 @@ function SideNav() {
       </a>
       {/* Sidebar */}
       <div className="sidebar">
-        {/* Sidebar user panel (optional) */}
         <div className="user-panel mt-3 pb-3 mb-3 d-flex">
           <div className="image">
             <img
