df4b6648867d71b17427213eb8f028676af76e83
Rohan Magar <rohan.magar.415@gmail.com>
2022-10-21 17:17:25 +0545

    added user sign in and sign up functionality

---
+++ server/controller/user.js
@@ -0,0 +1,103 @@
+const User = require("../model/user");
+const bcrypt = require("bcrypt");
+const { validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+
+exports.createUser = async (req, res, next) => {
+  const errors = validationResult(req);
+  const fname = req.body.fname;
+  const lname = req.body.lname;
+  const email = req.body.email;
+  const pass = req.body.password;
+  console.log(req.body);
+  console.log(fname, lname, email, pass);
+  let dupUser;
+  if (!errors.isEmpty()) {
+    const error = new Error("Validation failed!");
+    error.statusCode = 422;
+    throw error;
+  }
+  try {
+    dupUser = await User.findOne({ email: email });
+    if (dupUser) {
+      const error = new Error("Email Address already exists!");
+      error.statusCode = 409;
+      throw error;
+    }
+    const hashedPassword = await bcrypt.hash(pass, 12);
+    const user = new User({
+      fname: fname,
+      lname: lname,
+      email: email,
+      password: hashedPassword,
+    });
+    const result = await user.save();
+    // mailer.sendMail(
+    //   {
+    //     to: email,
+    //     from: "rohan.magar.415@gmail.com",
+    //     fromname: "Rohan Rana Magar",
+    //     subject: "Signup Succeeded!",
+    //     html: `<div style="text-align: center;">
+    //       <h2>Welcome ${name}</h2>
+    //       <p><span style="color:red;">Congratulations!!! </span>You have successfully signed up to online store.</p>
+    //     </div>`,
+    //   },
+    //   function (err, res) {
+    //     if (err) {
+    //       throw err;
+    //     }
+    //     console.log(res);
+    //   }
+    // );
+    res
+      .status(200)
+      .json({ message: "user created successfully", userId: result._id });
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    next(error);
+  }
+};
+
+exports.postLogin = async (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  console.log(email, password);
+
+  try {
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      const error = new Error("User does not Exists!");
+      error.statusCode = 401;
+      throw error;
+    }
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+      const error = new Error("Invalid UserName or Password");
+      error.statusCode = 401;
+      throw error;
+    }
+    const token = jwt.sign(
+      {
+        email: email,
+        userId: user._id.toString(),
+      },
+      process.env.JWT_TOKEN_SECRET,
+      {
+        expiresIn: "1h",
+      }
+    );
+    res.status(200).json({
+      message: "Login Successfull",
+      token: token,
+      userId: user._id.toString(),
+    });
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    next(error);
+  }
+};
---
+++ server/routes/user.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.Router();
+const userController = require("../controller/user");
+const { body } = require("express-validator");
+
+router.post(
+  "/",
+  [
+    body("fname")
+      .trim()
+      .not()
+      .isEmpty()
+      .matches(/^(?![\s.]+$)[a-zA-Z\s.]{2,}$/, "g"),
+    body("lname")
+      .trim()
+      .not()
+      .isEmpty()
+      .matches(/^(?![\s.]+$)[a-zA-Z\s.]{2,}$/, "g"),
+    body("email")
+      .trim()
+      .not()
+      .isEmpty()
+      .isEmail()
+      .withMessage("Please Enter a valid email."),
+    body("password")
+      .trim()
+      .not()
+      .isEmpty()
+      .matches(
+        /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*-_=?])[a-zA-Z0-9!@#$%^&*-_=?]{8,}/,
+        "g"
+      )
+      .isLength({ min: 8 }),
+  ],
+  userController.createUser
+);
+router.post("/login", userController.postLogin);
+
+module.exports = router;
