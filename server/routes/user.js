3926afe7520c64d817ab1238774cb127ee6bf664
Rohan Magar <rohan.magar.415@gmail.com>
2022-10-22 19:16:58 +0545

    adjusted signup functionality

--- server/routes/user.js
+++ server/routes/user.js
@@ -5,33 +5,33 @@ const { body } = require("express-validator");
 
 router.post(
   "/signup",
-  [
-    body("fname")
-      .trim()
-      .not()
-      .isEmpty()
-      .matches(/^(?![\s.]+$)[a-zA-Z\s.]{2,}$/, "g"),
-    body("lname")
-      .trim()
-      .not()
-      .isEmpty()
-      .matches(/^(?![\s.]+$)[a-zA-Z\s.]{2,}$/, "g"),
-    body("email")
-      .trim()
-      .not()
-      .isEmpty()
-      .isEmail()
-      .withMessage("Please Enter a valid email."),
-    body("password")
-      .trim()
-      .not()
-      .isEmpty()
-      .matches(
-        /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*-_=?])[a-zA-Z0-9!@#$%^&*-_=?]{8,}/,
-        "g"
-      )
-      .isLength({ min: 8 }),
-  ],
+  // [
+  //   body("fname")
+  //     .trim()
+  //     .not()
+  //     .isEmpty()
+  //     .matches(/^(?![\s.]+$)[a-zA-Z\s.]{2,}$/, "g"),
+  //   body("lname")
+  //     .trim()
+  //     .not()
+  //     .isEmpty()
+  //     .matches(/^(?![\s.]+$)[a-zA-Z\s.]{2,}$/, "g"),
+  //   body("email")
+  //     .trim()
+  //     .not()
+  //     .isEmpty()
+  //     .isEmail()
+  //     .withMessage("Please Enter a valid email."),
+  //   body("password")
+  //     .trim()
+  //     .not()
+  //     .isEmpty()
+  //     .matches(
+  //       /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*-_=?])[a-zA-Z0-9!@#$%^&*-_=?]{8,}/,
+  //       "g"
+  //     )
+  //     .isLength({ min: 8 }),
+  // ],
   userController.createUser
 );
 router.post("/login", userController.postLogin);
