1d75723a2ac31d567975d2697d44fed69db1e993
Roshan Shrestha <roshan.shrestha4u@gmail.com>
2022-10-29 13:52:41 +0545

    workspace ui updated

--- client/src/Pages/Dashboard/Company/Company.js
+++ client/src/Pages/Dashboard/Company/Company.js
@@ -6,6 +6,7 @@ import { ROUTES } from "../../../helper/routes";
 import { toast } from "react-toastify";
 
 import nameInitials from "name-initials";
+import Avatar from "../../../Components/UI/avatar/avatar";
 
 function Company() {
   const navigate = useNavigate();
@@ -16,7 +17,7 @@ function Company() {
       const response = await getAllCompanies();
       setCompanies(response?.result);
     } catch (e) {
-      toast(e);
+      toast.error(e);
       console.log(e);
     }
   };
@@ -25,39 +26,47 @@ function Company() {
     fetchCompanies();
   }, []);
 
+  console.log(companies);
+
   return (
     <div>
-      <div className="content-header">
+      <div className="content-header d-flex justify-content-between align-items-center">
         <h1 className="m-0">Workspace</h1>
+        <Button
+          variant="primary"
+          className="my-3"
+          onClick={() => navigate(ROUTES.CREATE_COMPANY)}
+        >
+          <i className="fa fa-plus"></i>
+          {""} Create your own workspace
+        </Button>
       </div>
 
       <table className="table">
         <thead>
           <th>Workspace Name</th>
+          <th>Available Space</th>
           <th>Created by</th>
+          <th>Address</th>
         </thead>
         <tbody>
           {companies?.map((company) => (
             <tr>
               <td>{company?.companyName || ""}</td>
+              <td>0/5</td>
               <td>
-                {nameInitials(
-                  `${company?.companyOwner?.fname} ${company?.companyOwner?.lname}`
-                )}
+                <Avatar
+                  initial={nameInitials(
+                    `${company?.companyOwner?.fname} ${company?.companyOwner?.lname}`
+                  )}
+                  name={`${company?.companyOwner?.fname} ${company?.companyOwner?.lname}`}
+                />
               </td>
+              <td>{`${company?.address?.street}, ${company?.address?.state}`}</td>
             </tr>
           ))}
         </tbody>
       </table>
-
-      <Button
-        variant="primary"
-        className="my-3"
-        onClick={() => navigate(ROUTES.CREATE_COMPANY)}
-      >
-        <i className="fa fa-plus"></i>
-        {""} Create your own workspace
-      </Button>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,9 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
 
-import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import "./Assets/css/bootstrap-icons.css";
-
-import "./Assets/css/landing.css";
+import "./Assets/css/index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
