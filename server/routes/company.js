d823cda49369cb1171add2ab21b882852578762e
Rohan Magar <rohan.magar.415@gmail.com>
2022-10-29 13:20:38 +0545

    implemented jwt verification

--- server/routes/company.js
+++ server/routes/company.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const companyController = require("../controller/company");
+const isAuth = require("../middleware/isAuth");
 
-router.get("/registercompany", companyController.getCompanies);
+router.get("/registercompany", isAuth, companyController.getCompanies);
 router.post("/registercompany", companyController.postCompanyInfo);
 router.post("/registercompany/:id", companyController.postCompanyFloorPlan);
 
