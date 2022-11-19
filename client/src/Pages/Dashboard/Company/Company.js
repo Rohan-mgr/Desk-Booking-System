a2e9eb22040c3e52d2157c6c5a93d64b0735b52a
Rohan Magar <rohan.magar.415@gmail.com>
2022-11-19 18:11:09 +0545

    implemented search functionality

--- client/src/Pages/Dashboard/Company/Company.js
+++ client/src/Pages/Dashboard/Company/Company.js
@@ -3,14 +3,11 @@ import React, { useState, useEffect } from "react";
 import { MdDelete, MdMoreVert, MdEdit, MdEventAvailable } from "react-icons/md";
 import { TbListDetails } from "react-icons/tb";
 import { AiFillSetting } from "react-icons/ai";
+import { IoMdClose } from "react-icons/io";
 import Button from "react-bootstrap/Button";
 import Dropdown from "react-bootstrap/Dropdown";
 import { useNavigate } from "react-router-dom";
-import {
-  getAllCompanies,
-  deleteCompany,
-  searchWorkspace,
-} from "../../../services/company";
+import { getAllCompanies, deleteCompany } from "../../../services/company";
 import { ROUTES } from "../../../helper/routes";
 import { toast } from "react-toastify";
 import Modal from "../../../Components/UI/Modal/AddFloor/AddModal";
@@ -38,7 +35,6 @@ function Company(props) {
       throw new Error(e);
     }
   };
-  console.log(companies);
 
   useEffect(() => {
     fetchCompanies();
@@ -47,26 +43,6 @@ function Company(props) {
   const closeModal = () => {
     setShowModal(false);
   };
-
-  useEffect(() => {
-    if (query.trim() === "" || query.length === 0) {
-      setCompanies(companies);
-    }
-    const data = companies?.filter((c) => {
-      return c.address.country.toLowerCase().includes(query);
-    });
-    setCompanies(data);
-  }, [query]);
-
-  // const handleChange = (e) => {
-  //   console.log(e.target.value);
-  //   if (e.target.value.trim() !== "") {
-  //   const data = companies?.filter((c) => {
-  //     return c.address.country.toLowerCase().includes(e.target.value);
-  //   });
-  //   setCompanies(data);
-  //   }
-  // };
 
   const handledeleteCompany = async (cid) => {
     try {
@@ -116,11 +92,24 @@ function Company(props) {
               <Form.Label>Search Workspace:</Form.Label>
               <Form.Control
                 className="col-7"
-                style={{ display: "inline-block" }}
+                style={{ display: "inline-block", position: "relative" }}
                 type="text"
+                value={query}
                 onChange={(e) => setQuery(e.target.value)}
-                placeholder="Searcy by country location"
+                placeholder="Searcy by country"
               />
+              {query.length > 0 && (
+                <IoMdClose
+                  onClick={() => setQuery("")}
+                  style={{
+                    position: "absolute",
+                    right: "28px",
+                    top: "22px",
+                    cursor: "pointer",
+                    fontSize: "1.5rem",
+                  }}
+                />
+              )}
             </Form.Group>
           </div>
         )}
@@ -138,71 +127,74 @@ function Company(props) {
           {/* {userMode === "company" && <th>Actions</th>} */}
         </thead>
         <tbody>
-          {companies?.map((company, _idx) => (
-            <tr key={company._id}>
-              <td>{_idx + 1}</td>
-              <td>
-                {" "}
-                <a
-                  href={`${ROUTES.COMPANY}/${ROUTES.COMPANY_INFO}/${company?._id}`}
-                >
-                  {company?.companyName || ""}
-                </a>{" "}
-              </td>
-              {/* <td>0/5</td> */}
-              <td>
-                <Avatar
-                  initial={nameInitials(
-                    `${company?.companyOwner?.fname} ${company?.companyOwner?.lname}`
-                  )}
-                  name={`${company?.companyOwner?.fname} ${company?.companyOwner?.lname}`}
-                />
-              </td>
-              {userMode === "company" && (
+          {companies
+            ?.filter((c) => c.address.country?.toLowerCase().includes(query))
+            .map((company, _idx) => (
+              <tr key={company._id}>
+                <td>{_idx + 1}</td>
                 <td>
-                  {new Date(company?.createdAt).toLocaleDateString("en-US")}
+                  {" "}
+                  <a
+                    href={`${ROUTES.COMPANY}/${ROUTES.COMPANY_INFO}/${company?._id}`}
+                  >
+                    {company?.companyName || ""}
+                  </a>{" "}
                 </td>
-              )}
-              {userMode === "user" && (
+                {/* <td>0/5</td> */}
                 <td>
-                  {company.floors
-                    .map((f) =>
-                      f.rooms
-                        .map(
-                          (r) =>
-                            r.desks.filter((d) => d.bookStatus === false).length
-                        )
-                        .reduce(
-                          (previousValue, currentValue) =>
-                            previousValue + currentValue,
-                          0
-                        )
-                    )
-                    .reduce(
-                      (previousValue, currentValue) =>
-                        previousValue + currentValue,
-                      0
+                  <Avatar
+                    initial={nameInitials(
+                      `${company?.companyOwner?.fname} ${company?.companyOwner?.lname}`
                     )}
-                  /
-                  {company.floors
-                    .map((f) =>
-                      f.rooms
-                        .map((d) => d.desks.length)
-                        .reduce(
-                          (previousValue, currentValue) =>
-                            previousValue + currentValue,
-                          0
-                        )
-                    )
-                    .reduce(
-                      (previousValue, currentValue) =>
-                        previousValue + currentValue,
-                      0
-                    )}
+                    name={`${company?.companyOwner?.fname} ${company?.companyOwner?.lname}`}
+                  />
                 </td>
-              )}
-              <td>{`${company?.address?.street}, ${company?.address?.state}`}</td>
-              {/* {userMode === "company" && (
+                {userMode === "company" && (
+                  <td>
+                    {new Date(company?.createdAt).toLocaleDateString("en-US")}
+                  </td>
+                )}
+                {userMode === "user" && (
+                  <td>
+                    {company.floors
+                      .map((f) =>
+                        f.rooms
+                          .map(
+                            (r) =>
+                              r.desks.filter((d) => d.bookStatus === false)
+                                .length
+                          )
+                          .reduce(
+                            (previousValue, currentValue) =>
+                              previousValue + currentValue,
+                            0
+                          )
+                      )
+                      .reduce(
+                        (previousValue, currentValue) =>
+                          previousValue + currentValue,
+                        0
+                      )}
+                    /
+                    {company.floors
+                      .map((f) =>
+                        f.rooms
+                          .map((d) => d.desks.length)
+                          .reduce(
+                            (previousValue, currentValue) =>
+                              previousValue + currentValue,
+                            0
+                          )
+                      )
+                      .reduce(
+                        (previousValue, currentValue) =>
+                          previousValue + currentValue,
+                        0
+                      )}
+                  </td>
+                )}
+                <td>{`${company?.address?.street}, ${company?.address?.country}`}</td>
+                {/* {userMode === "company" && (
                 <>
                   <td>
                     <Button
@@ -215,88 +207,92 @@ function Company(props) {
                 </>
               )} */}
 
-              <td>
-                <Dropdown>
-                  <Dropdown.Toggle
-                    as={CustomToggle}
-                    id="dropdown-custom-components"
-                  >
-                    <MdMoreVert />
-                  </Dropdown.Toggle>
+                <td>
+                  <Dropdown>
+                    <Dropdown.Toggle
+                      as={CustomToggle}
+                      id="dropdown-custom-components"
+                    >
+                      <MdMoreVert />
+                    </Dropdown.Toggle>
 
-                  <Dropdown.Menu style={{ marginBottom: "0 !important" }}>
-                    {userMode === "company" ? (
-                      <>
-                        <Dropdown.Item
-                          as="button"
-                          eventKey="1"
-                          onClick={() => {
-                            props.onSelectCompany(company);
-                            navigate(`${ROUTES.COMPANY_INFO}/${company?._id}`);
-                          }}
-                        >
-                          <div className="d-flex align-items-center text-primary">
-                            <TbListDetails className="mr-2" />{" "}
-                            <span style={{ marginTop: "1px" }}>
-                              View Details
-                            </span>
-                          </div>
-                        </Dropdown.Item>
-                        <Dropdown.Item
-                          as="button"
-                          eventKey="2"
-                          onClick={() => handleAddFloor(company)}
-                        >
-                          <div className="d-flex align-items-center text-primary">
-                            <AiFillSetting className="mr-2" />{" "}
-                            <span style={{ marginTop: "1px" }}>
-                              Manage Floor
-                            </span>
-                          </div>
-                        </Dropdown.Item>
-                        <Dropdown.Item as="button" eventKey="3">
-                          <div
-                            className="d-flex align-items-center text-primary"
-                            onClick={() =>
-                              handleEditCompany(company?._id, company)
-                            }
+                    <Dropdown.Menu style={{ marginBottom: "0 !important" }}>
+                      {userMode === "company" ? (
+                        <>
+                          <Dropdown.Item
+                            as="button"
+                            eventKey="1"
+                            onClick={() => {
+                              props.onSelectCompany(company);
+                              navigate(
+                                `${ROUTES.COMPANY_INFO}/${company?._id}`
+                              );
+                            }}
                           >
-                            <MdEdit className="mr-2" />{" "}
-                            <span style={{ marginTop: "2px" }}>Edit</span>
-                          </div>
-                        </Dropdown.Item>
-                        <Dropdown.Item as="button" eventKey="3">
-                          <div
-                            className="d-flex align-items-center text-danger"
-                            onClick={() => handledeleteCompany(company?._id)}
+                            <div className="d-flex align-items-center text-primary">
+                              <TbListDetails className="mr-2" />{" "}
+                              <span style={{ marginTop: "1px" }}>
+                                View Details
+                              </span>
+                            </div>
+                          </Dropdown.Item>
+                          <Dropdown.Item
+                            as="button"
+                            eventKey="2"
+                            onClick={() => handleAddFloor(company)}
                           >
-                            <MdDelete className="mr-2" />{" "}
-                            <span style={{ marginTop: "2px" }}>Delete</span>
-                          </div>
-                        </Dropdown.Item>
-                      </>
-                    ) : (
-                      <>
-                        <Dropdown.Item
-                          as="button"
-                          eventKey="1"
-                          onClick={() => {
-                            props.onSelectCompany(company);
-                            navigate(`${ROUTES.COMPANY_INFO}/${company?._id}`);
-                          }}
-                        >
-                          <div className="d-flex align-items-center text-primary">
-                            <MdEventAvailable className="mr-2" />{" "}
-                            <span>See Availability</span>
-                          </div>
-                        </Dropdown.Item>
-                      </>
-                    )}
-                  </Dropdown.Menu>
-                </Dropdown>
-              </td>
-            </tr>
-          ))}
+                            <div className="d-flex align-items-center text-primary">
+                              <AiFillSetting className="mr-2" />{" "}
+                              <span style={{ marginTop: "1px" }}>
+                                Manage Floor
+                              </span>
+                            </div>
+                          </Dropdown.Item>
+                          <Dropdown.Item as="button" eventKey="3">
+                            <div
+                              className="d-flex align-items-center text-primary"
+                              onClick={() =>
+                                handleEditCompany(company?._id, company)
+                              }
+                            >
+                              <MdEdit className="mr-2" />{" "}
+                              <span style={{ marginTop: "2px" }}>Edit</span>
+                            </div>
+                          </Dropdown.Item>
+                          <Dropdown.Item as="button" eventKey="3">
+                            <div
+                              className="d-flex align-items-center text-danger"
+                              onClick={() => handledeleteCompany(company?._id)}
+                            >
+                              <MdDelete className="mr-2" />{" "}
+                              <span style={{ marginTop: "2px" }}>Delete</span>
+                            </div>
+                          </Dropdown.Item>
+                        </>
+                      ) : (
+                        <>
+                          <Dropdown.Item
+                            as="button"
+                            eventKey="1"
+                            onClick={() => {
+                              props.onSelectCompany(company);
+                              navigate(
+                                `${ROUTES.COMPANY_INFO}/${company?._id}`
+                              );
+                            }}
+                          >
+                            <div className="d-flex align-items-center text-primary">
+                              <MdEventAvailable className="mr-2" />{" "}
+                              <span>See Availability</span>
+                            </div>
+                          </Dropdown.Item>
+                        </>
+                      )}
+                    </Dropdown.Menu>
+                  </Dropdown>
+                </td>
+              </tr>
+            ))}
         </tbody>
       </table>
       <Modal
